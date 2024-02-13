4d2898204f55b71028bc41a18fed95699980fb96
renft721 <renatoterreros95@gmail.com>
2024-02-13 20:19:12 +0100

    feat: integrar el wallet-adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material'
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'heavy-duty-dapp-root',
   template: `
-    <header>
-     <h1>Hola, soy renft.</h1>
+    <header class="py-8">
+     <h1 class="text-5xl text-center mb-4">Hola, soy renft.</h1>
+      <div class="flex justify-center">
+        <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
     </header>
     `,
 })
